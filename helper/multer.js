77c23df3496bfe6115ef215a2f696a7d681b3c41
verypei <verrymariyanto@gmail.com>
2022-07-02 08:45:08 +0700

    multer added

---
+++ helper/multer.js
@@ -0,0 +1,11 @@
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,path.join(__dirname, '../assets'))
+    },
+    filename:function(req,file,cb){
+        cb(null,  Date.now() + path.extname(file.originalname));
+    }
+})
+module.exports = storage;
