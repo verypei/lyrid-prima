74ccf4caa629cec49e676dfb4ba2a588c231dafe
verypei <verrymariyanto@gmail.com>
2022-07-02 07:28:33 +0700

    async await version

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,116 +1,75 @@
 require("dotenv").config();
-const {Products, Users} = require("../models");
+const { Products } = require("../models");
 
-class Controller{
-
-    static getProducts(req,res){
-        Products.findAll({
-                limit: 5,
-                offset: (req.query.page - 1) * 5,
-                order: [
-                    ['nama_produk', 'ASC'],
-                ]
-            })
-        .then(data=>{
-            res.status(200).json(data);
-        })
-        .catch(err=>{
-            res.status(500).json(err);
-        })
+class Controller {
+  static async getProducts(req, res) {
+    try {
+      let data = await Products.findAll({
+        limit: 5,
+        offset: (req.query.page - 1) * 5,
+        order: [["id", "ASC"]],
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json(err);
     }
+  }
 
-    static getProductById(req,res){
-        let id = req.params.id;
-        Products.findOne({where:{id}})
-        .then(data=>{
-            res.status(200).json({data})
-        })
-        .catch(err=>{
-            res.status(404).json({message:"data not found!!!"})
-        })
+  static async getProductById(req, res) {
+    let id = req.params.id;
+    try {
+      let data = await Products.findOne({ where: { id } });
+      res.status(200).json({ data });
+    } catch (error) {
+      res.status(404).json({ message: "data not found!!!" });
     }
+  }
 
-    static addProduct(req,res){
-        let obj = {
-            kode_produk:req.body.kode_produk,
-            nama_produk:req.body.nama_produk,
-            qty:req.body.qty,
-            image_produk:req.body.image_produk
-        }
-        let userName = ""
-        Users.findOne({where:{id:req.user.id}})
-        .then(dataUser=>{
-            userName = dataUser.user_name
-        })
-        .catch(err=>{
-            console.log(err,"from add pray in controllers");
-        })
-
-        Products.create(obj)
-        .then(data=>{
-            res.status(201).json(data)
-        })
-        .catch(err=>{
-            if(err.errors){
-                res.status(400).json(errData)
-            }
-            else{
-                res.status(500).json(err)
-            }
-
-        })
+  static async addProduct(req, res) {
+    let obj = {
+      kode_produk: req.body.kode_produk,
+      nama_produk: req.body.nama_produk,
+      qty: req.body.qty,
+      image_produk: req.body.image_produk,
+    };
+    try {
+      let data = await Products.create(obj);
+      res.status(201).json(data);
+    } catch (error) {
+      res.status(500).json(err);
     }
+  }
 
-    static updateProduct(req,res){
-        let id = req.params.id
-        console.log(req.body,"-----controller");
-        let newData={
-            kode_produk:req.body.kode_produk,
-            nama_produk:req.body.nama_produk,
-            qty:req.body.qty,
-            image_produk:req.body.image_produk
-        }
-        console.log(req.user.id,"update=========================",id,"---",newData);
-        Products.update(newData,{where:{id:id}})
-        .then(resp=>{
-            if(resp){
-                res.status(200).json(resp)
-            }
-            else{
-                res.status(404).json({
-                    message:"data can't be updated / not found"
-                })
-            }
-        })
-        .catch(err=>{
-            if(err.errors){
-                let errData= [];
-                for(let i=0;i<err.errors.length;i++){
-                    errData.push({message:err.errors[i].message})
-                }
-                res.status(400).json(errData)
-            }
-            else{
-                res.status(500).json(err)
-            }
-
-        })
+  static async updateProduct(req, res) {
+    let id = req.params.id;
+    let newData = {
+      kode_produk: req.body.kode_produk,
+      nama_produk: req.body.nama_produk,
+      qty: req.body.qty,
+      image_produk: req.body.image_produk,
+    };
+    try {
+      let data = await Products.update(newData, { where: { id } });
+      data
+        ? res.status(200).json({messsage : "data updated"})
+        : res.status(404).json({
+            message: "data can't be updated / not found",
+          });
+    } catch (error) {
+          res.status(500).json(error);
     }
+  }
 
-    static deleteProduct(req,res){
-        let id = req.params.id;
-        Products.destroy({where:{id:id}})
-        .then(data=>{
-            if(data){
-                res.status(201).json({message:"Data has been removed"})
-            }
-            else{
-                res.status(404).json({message:"error data not found"})
-            }
-        })
-        .catch(err=>{
-            res.status(500).json(err)
-        })          
+  static async deleteProduct(req, res) {
+    let id = req.params.id;
+    try {
+      let data = await Products.destroy({ where: { id } });
+      data
+        ? res.status(201).json({ message: "Data has been removed" })
+        : res.status(404).json({ message: "error data not found" });
+    } catch (error) {
+      res.status(500).json(err);
     }
+  }
 }
-module.exports=Controller;
+module.exports = Controller;
--- controllers/userController.js
+++ controllers/userController.js
@@ -6,62 +6,43 @@ const Authentication = require("../middleware/authentication");
 
 class userController{
 
-    static register(req,res){
-        
+    static async register(req,res){
         let obj = {
             email:req.body.email,
             first_name:req.body.first_name,
             last_name:req.body.last_name,
             password:req.body.password
         }
-        console.log(obj,"----->>>>");
-// ====================================cari email apakah sudah terdaftar=============================
-        Users.findOne({where:{email:obj.email}})
-        .then(data=>{
-            if(!data){
-                Users.create(obj)
-                .then(data=>{
-                    let token = jwt.sign({id: data.id, email: data.email}, process.env.JWT_SECRET)
-                    res.status(201).json({token})
-                })
-                .catch(err=>{
-                    console.log(err);
-                    res.status(500).json(err);
-                })
-            }
+        try {
+            let email = await Users.findOne({where:{email:obj.email}});
+            if(!email){
+                let data = Users.create(obj);
+                let token = jwt.sign({id: data.id, email: data.email}, process.env.JWT_SECRET);
+                res.status(201).json({token})
+            } 
             else{
-                res.status(403).json({message:"email already in used"});
-            }
-        })
-        .catch(err=>{
-            res.status(500).json(err);
-        })
+                res.status(422).json({message : "email already taken"})
+            } 
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
 
-    static login(req,res){
+    static async login(req,res){
         let email=req.body.email
         let password=req.body.password
-        Users.findOne({
-            where:{
-                email:email
-            }
-        })
-        .then(data=>{
-            if(!data){
-                res.status(400).json('email wrong')
+        let data = await Users.findOne({where:{email}})
+        if(data){
+            if(bcrypt.compareSync(password, data.password)){
+                let token = jwt.sign({id: data.id, email: data.email}, process.env.JWT_SECRET)
+                res.status(200).json({token})
             } else {
-                console.log(data,"-----");
-                if(bcrypt.compareSync(password, data.password)){
-                    let token = jwt.sign({id: data.id, email: data.email}, process.env.JWT_SECRET)
-                    res.status(200).json({token})
-                } else {
-                    res.status(400).json({message: 'wrong password'});
-                }
+                res.status(400).json({message: 'wrong password'});
             }
-        })
-        .catch(err=>{
+        }
+        else{
             res.status(404).json({message : "email not found"});
-        })
+        }
     }
 }
 module.exports=userController
